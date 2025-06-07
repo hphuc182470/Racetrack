1d1c6ce35b03e7ef49345eacccaf311ad7a6c4f3
Minh An <andmse182449@fpt.edu.vn>
2025-06-07 07:57:30 +0700

    Intergrate tutorial page to main

--- app/src/main/java/com/example/lab4/TutorialActivity.java
+++ app/src/main/java/com/example/lab4/TutorialActivity.java
@@ -1,6 +1,8 @@
 package com.example.lab4;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class TutorialActivity extends AppCompatActivity {
+    Button buttonGotIt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tutorial);
+
+        buttonGotIt = findViewById(R.id.buttonGotIt);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_tutorial_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        buttonGotIt.setOnClickListener(v -> finish());
     }
 }
